2a9a2fe71424284d723dc7dcf4c925aabc16925d
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-08-21 10:50:38 -0300

    Criando as funções para as rotas das páginas

--- app/router/router.php
+++ app/router/router.php
@@ -4,16 +4,33 @@ function routes() {
     return require "./routes.php";
 }
 
+// Função que trabalha com as URIs exatas
 function exactMatchUriInArrayRoutes($uri, $routes) {
     if (array_key_exists($uri, $routes)) {
-        return [];
+        return [$uri => $routes[$uri]];
     }
 
     return [];
 }
 
+// Função que trabalha com as URIs dinâmicas
+function regularExpressionMatchArrayRoutes($uri, $routes) {
+    return array_filter(
+        $routes,
+        function($value) use($uri) {
+            $regex = str_replace('/', '\/', ltrim($value, '/'));
+            return preg_match("/^$regex$/", ltrim($uri, '/'));
+        },
+        ARRAY_FILTER_USE_KEY
+    );
+}
+
+// Função que executa e verifica as rotas do projeto
 function router() {
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $routes = routes();
     $matchedUri = exactMatchUriInArrayRoutes($uri,$routes);
+    if (empty($matchedUri)) {
+        $matchedUri = regularExpressionMatchArrayRoutes($uri,$routes);
+    }
 }
