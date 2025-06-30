20b7f82c1016f0da5fd7ad87669bab547919a22b
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-29 21:38:41 -0300

    create params function for the routes

--- app/router/router.php
+++ app/router/router.php
@@ -24,12 +24,25 @@ function regularExpressionMatchArrayRoutes($uri, $routes) {
     );
 }
 
+function params($uri, $matchedUri) {
+    if (!empty($matchedUri)) {
+        $matchedToGetParams = array_keys($matchedUri)[0];
+        return array_diff(
+            explode('/', ltrim($uri, '/')),
+            explode('/', ltrim($matchedToGetParams, '/'))
+        );    
+    }
+    return [];
+}
+
 // Função que executa e verifica as rotas do projeto
 function router() {
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $routes = routes();
+
     $matchedUri = exactMatchUriInArrayRoutes($uri, $routes);
     if (empty($matchedUri)) {
         $matchedUri = regularExpressionMatchArrayRoutes($uri, $routes);
+        $params = params($uri, $matchedUri);
     }
 }
