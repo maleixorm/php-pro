7f0f245ed4a3da8a5142a27a21cec12277aca98c
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-30 16:18:57 -0300

    adjust routes

--- app/router/routes.php
+++ app/router/routes.php
@@ -3,6 +3,6 @@
 return [
     '/php-pro/public/' => 'Home@index',
     '/php-pro/public/user/create' => 'User@create',
-    '/php-pro/public/user/[a-z0-9]+' => 'User@index',
-    '/php-pro/public/user/[a-z0-9]+/name/[a-z]+' => 'User@show'
+    '/php-pro/public/user/[a-z0-9]+' => 'User@show',
+    '/php-pro/public/user/[a-z0-9]+/name/[a-z]+' => 'User@create'
 ];
