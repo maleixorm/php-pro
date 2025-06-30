f60cd423569fd31d660f67ac8b6898da6e1ad1a3
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-30 12:41:01 -0300

    att autoload

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -8,4 +8,5 @@ $baseDir = dirname($vendorDir);
 return array(
     '994b6b371aba2e0458de0eccc74abba0' => $baseDir . '/app/helpers/constants.php',
     'd9325a911edd1fe76223d7300001474b' => $baseDir . '/app/router/router.php',
+    'a0e6ff9a9d1f9b70c911e76fb63be03f' => $baseDir . '/app/core/controller.php',
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -9,6 +9,7 @@ class ComposerStaticInitf5e4edd9a1157e258fa435e9711b540c
     public static $files = array (
         '994b6b371aba2e0458de0eccc74abba0' => __DIR__ . '/../..' . '/app/helpers/constants.php',
         'd9325a911edd1fe76223d7300001474b' => __DIR__ . '/../..' . '/app/router/router.php',
+        'a0e6ff9a9d1f9b70c911e76fb63be03f' => __DIR__ . '/../..' . '/app/core/controller.php',
     );
 
     public static $prefixLengthsPsr4 = array (
