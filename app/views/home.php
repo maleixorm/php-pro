acb6d6555e51615db459e083becf47c76b164d5e
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-02 15:09:53 -0300

    Listando dados na view home

--- app/views/home.php
+++ app/views/home.php
@@ -1,2 +1,27 @@
-<h2>Home</h2>
-<h3>Curso de PHP Profissional</h3>
+<h2>Usuários</h2>
+<div
+    class="table-responsive"
+>
+    <table
+        class="table table-striped table-hover"
+    >
+        <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Email</th>
+                <th scope="col">Detalhes</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($users as $user): ?>
+                <tr class="">
+                    <td scope="row"><?= $user->id; ?></td>
+                    <td><?= $user->name; ?></td>
+                    <td><?= $user->email; ?></td>
+                    <td><a href="/php-pro/public/user/<?= $user->id; ?>" class="btn btn-primary"><i class="bi bi-pencil-square"></i></a></td>
+                </tr>
+            <?php endforeach; ?>    
+        </tbody>
+    </table>
+</div>
