4a820ae6bf30b88d0c232a949a2240ac6461da82
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-02 13:32:25 -0300

    validando o índice data

--- public/index.php
+++ public/index.php
@@ -4,13 +4,16 @@ require "bootstrap.php";
 
 try {
     $data = router();
-    extract($data['data']);
+    if (!isset($data['data'])) {
+        throw new Exception('O índice data está faltando.');
+    }
     if (!isset($data['view'])) {
         throw new Exception('O índice view está faltando.');
     }
     if (!file_exists(VIEWS.$data['view'])) {
         throw new Exception("Essa view {$data['view']} não existe.");
     }
+    extract($data['data']);
     $view = $data['view'];
     require ROOT."/app/views/master.php";
 } catch (\Exception $e) {
