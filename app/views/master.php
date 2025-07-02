b07a15283f56e12db25b8b19bf5a6c1f1940baa4
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-02 13:33:30 -0300

    Deixando os títulos das páginas dinâmico

--- app/views/master.php
+++ app/views/master.php
@@ -3,10 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Curso de PHP Profissional</title>
+    <title><?php echo $title; ?></title>
 </head>
 <body>
-    <h2><?php echo $name; ?></h2>
     <div class="container">
         <?php require VIEWS.$view; ?>
     </div>
