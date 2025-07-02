d70218ca886ff0299bd2691f2600a639e35a0798
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-02 15:09:32 -0300

    Ajustando as rotas do projeto

--- app/router/routes.php
+++ app/router/routes.php
@@ -3,6 +3,5 @@
 return [
     '/php-pro/public/' => 'Home@index',
     '/php-pro/public/user/create' => 'User@create',
-    '/php-pro/public/user/[a-z0-9]+' => 'User@show',
-    '/php-pro/public/user/[a-z0-9]+/name/[a-z]+' => 'User@create'
+    '/php-pro/public/user/[a-z0-9]+' => 'User@show'
 ];
