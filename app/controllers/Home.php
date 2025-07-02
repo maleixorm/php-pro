208bc45ff156384903608aa850eff71791d145ad
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-02 15:07:37 -0300

    Adicionando dados da função all à view

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -6,9 +6,10 @@ class Home
 {
     public function index($params)
     {
+        $users = all('users');
         return [
             'view' => 'home.php',
-            'data' => ['title' => 'Home']
+            'data' => ['title' => 'Home', 'users' => $users]
         ];
     }
 }
