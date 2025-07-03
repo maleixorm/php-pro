a7ebee50b6f360a661a25088c9b347ecb6701482
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-03 18:50:20 -0300

    Separando as rotas em métodos

--- app/router/routes.php
+++ app/router/routes.php
@@ -1,7 +1,13 @@
 <?php
 
 return [
-    '/php-pro/public/' => 'Home@index',
-    '/php-pro/public/user/create' => 'User@create',
-    '/php-pro/public/user/[a-z0-9]+' => 'User@show'
+    'POST' => [
+        '/php-pro/public/login' => 'Login@store'
+    ],
+    'GET' => [
+        '/php-pro/public/' => 'Home@index',
+        '/php-pro/public/user/create' => 'User@create',
+        '/php-pro/public/user/[a-z0-9]+' => 'User@show',
+        '/php-pro/public/login' => 'Login@index'
+    ]
 ];
