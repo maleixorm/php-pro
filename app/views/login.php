19ad180ebdd699fb138790783bc15ead229cd5cf
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-03 18:51:07 -0300

    Criando a view de login

---
+++ app/views/login.php
@@ -0,0 +1,9 @@
+<h2 class="text-center my-4">Login</h2>
+
+<form action="/php-pro/public/login" method="post" class="form-control mb-3">
+    <label for="email" class="form-label">Email: </label>
+    <input type="email" name="email" id="" class="form-control mb-3">
+    <label for="password" class="form-label">Password: </label>
+    <input type="password" name="password" id="" class="form-control mb-3">
+    <button type="submit" class="btn btn-primary">Entrar</button>
+</form>
