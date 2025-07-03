6282b2341c1dfbf2f6931b10f7d46f1e96ee98de
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-03 18:52:08 -0300

    criando o header da home

---
+++ app/views/partials/header.php
@@ -0,0 +1,9 @@
+<ul id="menu_list">
+    <li><a href="/php-pro/public">Home</a></li>
+    <li><a href="/php-pro/public/login">Login</a></li>
+    <li><a href="/php-pro/public/user/create">Create</a></li>
+</ul>
+
+<div id="status_login">
+    Bem-vindo, visitante!
+</div>
